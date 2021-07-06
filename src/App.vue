e5f765d3cf0b2a0f2e974c02b57381ac17b0d960
yinyou <1337374876@qq.com>
2021-07-06 14:23:05 +0800

    删除.idea

--- src/App.vue
+++ src/App.vue
@@ -35,7 +35,7 @@
 	  font-family: Avenir, Helvetica, Arial, sans-serif;
 	  -webkit-font-smoothing: antialiased;
 	  -moz-osx-font-smoothing: grayscale;
-	  color: #2c3e50;
+	  color: #502c31;
 	  width: 100%;
 	  height: 100%;
 	  position: fixed;
