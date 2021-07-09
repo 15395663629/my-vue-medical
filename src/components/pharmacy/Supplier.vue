53bd1638c584564c2adf6237510dc1053c98ed3d
JIANG <13874192491@163.com>
2021-07-09 12:15:32 +0800

    供应商管理

--- src/components/pharmacy/Supplier.vue
+++ src/components/pharmacy/Supplier.vue
@@ -1,31 +1,31 @@
 <template>
   <h1>供应商管理</h1>
   <el-row>
-    <el-col :offset="10">
+    <el-col>
       <!-- 新增弹窗 -->
       <el-button type="primary" @click="dialogFormVisible = true">新增供应商</el-button>
       <el-dialog  title="供应商" v-model="dialogFormVisible" width="20%">
-        <el-form :model="tableData">
+        <el-form :model="formSipplier">
           <el-form-item label="供应商编号" :label-width="formLabelWidth">
-            <el-input v-model="tableData.name" autocomplete="off" style="width: 200px;"></el-input>
+            <el-input v-model="formSipplier.supplierId" autocomplete="off" style="width: 200px;"></el-input>
           </el-form-item>
           <el-form-item label="供应商名称" :label-width="formLabelWidth">
-            <el-input v-model="tableData.guige" autocomplete="off" style="width: 200px;"></el-input>
+            <el-input v-model="formSipplier.supplierName" autocomplete="off" style="width: 200px;"></el-input>
           </el-form-item>
           <el-form-item label="供应商地址" :label-width="formLabelWidth">
-            <el-input v-model="tableData.danwei" type="textarea" autocomplete="off" style="width: 200px;"></el-input>
+            <el-input v-model="formSipplier.supplierSite" type="textarea" autocomplete="off" style="width: 200px;"></el-input>
           </el-form-item>
           <el-form-item label="供应商电话" :label-width="formLabelWidth">
-            <el-input v-model="tableData.chuku" type="text" autocomplete="off" style="width: 200px;"></el-input>
+            <el-input v-model="formSipplier.supplierPhone" type="text" autocomplete="off" style="width: 200px;"></el-input>
           </el-form-item>
           <el-form-item label="供应商联系人" :label-width="formLabelWidth">
-            <el-input v-model="tableData.ruku" type="text" autocomplete="off" style="width: 200px;"></el-input>
+            <el-input v-model="formSipplier.supplierLinkman" type="text" autocomplete="off" style="width: 200px;"></el-input>
           </el-form-item>
         </el-form>
         <template #footer>
 				<span class="dialog-footer">
-				  <el-button @click="dialogFormVisible = false">取 消</el-button>
-				  <el-button type="primary" @click="dialogFormVisible = false">确 定</el-button>
+				  <el-button @click="clearform()">取 消</el-button>
+				  <el-button type="primary" @click="addSupplier()">确 定</el-button>
 				</span>
         </template>
       </el-dialog>
@@ -33,52 +33,56 @@
       <!--修改弹窗-->
 
       <el-dialog  title="供应商" v-model="handleEdit" width="20%">
-        <el-form :model="tableData">
-          <el-form-item label="供应商编号" :label-width="formLabelWidth">
-            <el-input v-model="tableData.name" autocomplete="off" style="width: 200px;"></el-input>
-          </el-form-item>
+        <el-form :model="formSipplier">
+          <el-row :gutter="0">
+           <el-col :span="0">
+             <el-form-item label="供应商编号" :label-width="formLabelWidth">
+               <el-input v-model="formSipplier.supplierId" autocomplete="off" style="width: 200px;"></el-input>
+             </el-form-item>
+           </el-col>
+          </el-row>
+
           <el-form-item label="供应商名称" :label-width="formLabelWidth">
-            <el-input v-model="tableData.guige" autocomplete="off" style="width: 200px;"></el-input>
+            <el-input v-model="formSipplier.supplierName" autocomplete="off" style="width: 200px;"></el-input>
           </el-form-item>
           <el-form-item label="供应商地址" :label-width="formLabelWidth">
-            <el-input v-model="tableData.danwei" type="textarea" autocomplete="off" style="width: 200px;"></el-input>
+            <el-input v-model="formSipplier.supplierSite" type="textarea" autocomplete="off" style="width: 200px;"></el-input>
           </el-form-item>
           <el-form-item label="供应商电话" :label-width="formLabelWidth">
-            <el-input v-model="tableData.chuku" type="text" autocomplete="off" style="width: 200px;"></el-input>
+            <el-input v-model="formSipplier.supplierPhone" type="text" autocomplete="off" style="width: 200px;"></el-input>
           </el-form-item>
           <el-form-item label="供应商联系人" :label-width="formLabelWidth">
-            <el-input v-model="tableData.ruku" type="text" autocomplete="off" style="width: 200px;"></el-input>
+            <el-input v-model="formSipplier.supplierLinkman" type="text" autocomplete="off" style="width: 200px;"></el-input>
           </el-form-item>
         </el-form>
         <template #footer>
           <span class="dialog-footer">
-            <el-button  @click="handleEdit = false">取 消</el-button>
-            <el-button type="primary" @click="handleEdit = false">确 定</el-button>
+            <el-button  @click="clearform()">取 消</el-button>
+            <el-button type="primary" @click="xiugai()">确 定</el-button>
           </span>
         </template>
       </el-dialog>
-
     </el-col>
   </el-row>
-  <!-- 盘点表格 -->
-  <el-table :data="tableData.filter(data => !search || data.name.toLowerCase().includes(search.toLowerCase()))"
+  <!-- 供应商表格 -->
+  <el-table :data="tableData.filter(data => !search || data.supplierName.toLowerCase().includes(search.toLowerCase()))"
             style="width: 100%">
-    <el-table-column label="供应商编号" prop="name">
+    <el-table-column label="供应商编号" prop="supplierId">
     </el-table-column>
-    <el-table-column label="供应商名称" prop="guige">
+    <el-table-column label="供应商名称" prop="supplierName">
     </el-table-column>
-    <el-table-column label="供应商地址" prop="danwei">
+    <el-table-column label="供应商地址" prop="supplierSite">
     </el-table-column>
-    <el-table-column label="供应商电话" prop="chuku">
+    <el-table-column label="供应商电话" prop="supplierPhone">
     </el-table-column>
-    <el-table-column label="供应商联系人" prop="ruku">
+    <el-table-column label="供应商联系人" prop="supplierLinkman">
     </el-table-column>
     <el-table-column align="right">
       <template #header>
         <el-input v-model="search" size="mini" placeholder="输入关键字搜索" />
       </template>
       <template #default="scope">
-        <el-button size="mini" @click="handleEdit = true">编辑供应商</el-button>
+        <el-button size="mini" @click="editSupplier(scope.row)">编辑供应商</el-button>
         <el-button size="mini" type="danger" @click="handleDelete">删除供应商</el-button>
       </template>
     </el-table-column>
@@ -100,22 +104,14 @@
 export default {
   data() {
     return {
-      tableData: [
-          {
-        name: '板蓝根',
-        guige: '20g/包',
-        danwei: '1/盒',
-        chuku: '40',
-        ruku: '60'
-          },
-        {
-          name: '奥利弗',
-          guige: '20g/包',
-          danwei: '1/盒',
-          chuku: '40',
-          ruku: '60'
-        },
-      ],
+      formSipplier:{
+        supplierId:'',
+        supplierName:'',
+        supplierSite:'',
+        supplierPhone:'',
+        supplierLinkman:''
+      },
+      tableData: [],
       search: '',
       // 新增弹窗
       dialogFormVisible: false,
@@ -134,7 +130,71 @@ export default {
     },
     handleCurrentChange(val) {
       console.log(`当前页: ${val}`);
+    },
+    getData(){
+      this.axios({
+        url:'http://localhost:8089/gys-all',
+        params:{}
+      }).then((v)=>{
+        console.log(v)
+        this.tableData = v.data;
+      }).catch(function (){
+
+      })
+    },
+    //新增
+    addSupplier(){
+      console.log(this.formSipplier);
+      this.axios({
+        url:'http://localhost:8089/gys-add',
+        params:{ykSupplier:this.formSipplier}
+      }).then((v)=>{
+        this.getData();
+        this.clearform();
+      }).catch(function (){
+
+      })
+    },
+    //修改获取数据
+    editSupplier(row){
+      console.log(row.supplierId)
+      this.handleEdit = true;
+      this.formSipplier={
+        supplierId:row.supplierId,
+            supplierName:row.supplierName,
+            supplierSite:row.supplierSite,
+            supplierPhone:row.supplierPhone,
+            supplierLinkman:row.supplierLinkman
+      }
+    },
+    /*清空表单*/
+    clearform(){
+      this.formSipplier={
+        supplierId:'',
+            supplierName:'',
+            supplierSite:'',
+            supplierPhone:'',
+            supplierLinkman:''
+      }
+      this.dialogFormVisible=false
+      this.handleEdit = false
+    },
+    /*修改*/
+    xiugai(){
+      console.log(this.formSipplier);
+      this.axios({
+        url:'http://localhost:8089/gys-update',
+        params:{ykSupplier:this.formSipplier}
+      }).then((v)=>{
+        this.getData();
+        this.clearform();
+      }).catch(function (){
+
+      })
     }
+  },
+  created() {
+    this.getData()
   }
 }
 </script>
