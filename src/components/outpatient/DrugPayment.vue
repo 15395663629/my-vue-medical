439be26eae3fe72e253480d15be7cb61d88d5ab6
shizi <2054701360@qq.com>
2021-09-02 08:44:58 +0800

    医嘱管理5.0

--- src/components/outpatient/DrugPayment.vue
+++ src/components/outpatient/DrugPayment.vue
@@ -1,8 +1,6 @@
 <template><!-- 药品缴费 -->
-  430224199805045517
-  {{leftTableList}}
   <el-row>
-    <el-col :span="10"  style="margin-top: 20px">
+    <el-col :span="10"  style="margin-top: 10px">
       <el-row >
         <el-col :span="24">
           <el-input size="small" style="width: 200px;" v-model="leftText"  placeholder="请输入你要查询的内容" ></el-input>
@@ -51,8 +49,9 @@
           </el-col >
         </el-form>
 
-        <el-table show-summary size="mini" height="370" row-key="date" style="margin-top: 25px" :data="leftTableList">
-          <el-table-column fixed prop="xmName"  align="center" label="项目类型"  filter-placement="bottom-end">
+        <el-table  size="mini" height="380" row-key="date" style="margin-top: 25px" :data="leftTableList">
+          
+		  <el-table-column fixed prop="xmName"  align="center" label="项目类型"  filter-placement="bottom-end">
             <template #default="scope">
               <el-tag size="small"  :type="colorType(scope.row.xmName)" disable-transitions>
                 {{scope.row.xmName}}
@@ -67,7 +66,9 @@
           </el-table-column>
           <el-table-column fixed  label="项目留言"  align="center" >
             <template #default="scope">
-              {{scope.row.xmText}}
+				<div class="fontType">
+					{{scope.row.xmText}}
+				</div>
             </template>
           </el-table-column>
 
@@ -99,7 +100,7 @@
 
           <el-table-column fixed  label="操作" align="center" >
             <template #default="scope">
-              <el-button type="success" size="mini" plain @click="forPrinting(scope.row.xmNumber)">收费打印</el-button>
+              <el-button type="success" size="mini" plain @click="forPrinting(scope.row.xmNumber,scope.row.xmName)">收费打印</el-button>
             </template>
           </el-table-column>
 
@@ -108,32 +109,32 @@
 
     </el-col>
 <!--    缴费查询   -->
-    <el-col :span="14" style="margin-top: 20px">
+    <el-col :span="14" style="margin-top: 10px">
       <el-row>
         <el-col :span="24" style="margin-left: 10px">
-          <el-input size="small" style="width: 200px;" placeholder="请输入你要查询的内容" ></el-input>
-          <el-button size="small" type="primary" icon="el-icon-search">查询</el-button>
+          <el-input size="small" style="width: 200px;" v-model="rightText" placeholder="请输入你要查询的内容" ></el-input>
+          <el-button size="small" type="primary" icon="el-icon-search" @click="selectRecordsAll">查询</el-button>
         </el-col>
-        <el-table size="mini"  height="560px" row-key="date"  :data="tableData" style="width: 100% ;margin-top: 5px;margin-left: 10px">
+        <el-table size="mini"  height="570px" row-key="date"  :data="rightList" style="width: 100% ;margin-top: 5px;margin-left: 10px">
 
-          <el-table-column fixed label="序号"> </el-table-column>
-          <el-table-column fixed label="就诊卡号"> </el-table-column>
-          <el-table-column fixed label="姓名"> </el-table-column>
-          <el-table-column fixed label="项目总览">
-            <template #default="scope">
-              <el-popover effect="light" trigger="hover" placement="top">
-                <template #default>
-                  <p>嘱托详情: {{ scope.row.address }}</p>
-                </template>
-                <template #reference>
-                  <div  class="name-wrapper">
-                    <el-tag  size="small">{{ scope.row.address }}</el-tag>
-                  </div>
+			<el-table-column fixed label="序号" prop="mrNumber"> </el-table-column>
+			<el-table-column fixed label="就诊卡号" prop="mrMcCard"> </el-table-column>
+			<el-table-column fixed label="姓名" prop="recipeObject.recipeSickName"></el-table-column>
+			<el-table-column fixed label="项目总览">
+				<template #default="scope">
+				<el-popover effect="light" trigger="hover" placement="top">
+				<template #default>
+					<p>嘱托详情: {{ scope.row.address}}</p>
+				</template>
+				<template #reference>
+					<div  class="name-wrapper">
+						<el-tag  size="small">{{ scope.row.address }}</el-tag>
+					</div>
                 </template>
               </el-popover>
             </template>
           </el-table-column>
-          <el-table-column fixed label="项目总金额"> </el-table-column>
+          <el-table-column fixed label="项目总金额" prop="mrTotalMoney"></el-table-column>
           <el-table-column fixed  label="操作">
             <template #default="scope">
               <el-tooltip content="打印小票" >
@@ -158,6 +159,7 @@
 </template>
 
 <script>
+	
 	export default {
 		data() {
 			return {
@@ -170,134 +172,175 @@
 					drugType:'中药'
 				},
 				],
-        //输入text
-        leftText:'',
+				//输入text
+				leftText:'',
 
-        rightText:'',
-        //显示的病人对象
-        leftRecordObject:{
-          mrCount:'',
-          recipeSickName:'',
-          mrIdCard:'',
-          mrMcCard:'',
-          sickPhone:'',
-          mrKsName:'',
-          mrDoctorName:'',
-          mrTotalMoney:'',
-        },
-        leftRecordList:'',
-        //项目集合
-        leftTableList:[],
-        //西药处方
-        xpObject:{
-          xmNumber:0,
-          xmName:'',
-          xmSum:'',
-          xmText:'',
-          xmContent:[],
-        },
-        //中药处方
-        zpObject:{
+				rightText:'',
+				//已缴费人员的集合
+				rightList:[],
+				//显示的病人对象
+				leftRecordObject:{
+				  mrCount:'',
+				  recipeSickName:'',
+				  mrIdCard:'',
+				  mrMcCard:'',
+				  sickPhone:'',
+				  mrKsName:'',
+				  mrDoctorName:'',
+				  mrTotalMoney:'',
+				},
+				leftRecordList:'',
+				//项目集合
+				leftTableList:[],
+				//西药处方
+				xpObject:{
 				  xmNumber:0,
-          xmName:'',
-          xmSum:'',
-          xmText:'',
-          xmContent:[],
-        },
-        //药品赋值对象
-        contentObject:{},
-        //西药暂用数组
-        contentArr1:[],
-        //中药暂用数组
-        contentArr2:[],
+				  xmName:'',
+				  xmSum:'',
+				  xmText:'',
+				  xmContent:[],
+				},
+				//中药处方
+				zpObject:{
+						  xmNumber:0,
+				  xmName:'',
+				  xmSum:'',
+				  xmText:'',
+				  xmContent:[],
+				},
+				//药品赋值对象
+				contentObject:{},
+				//西药暂用数组
+				contentArr1:[],
+				//中药暂用数组
+				contentArr2:[],
 			}
 		},
 		 methods: {
 		  /*搜索输入*/
-       selectRecord(){
-         if(this.leftText!=null){
-           // 每次添加之前先清空一边集合
-           this.leftTableList=[];
-           this.axios.post("selectAllRecords",{texts:this.leftText}).then((v)=>{
-             //赋值到个人信息去
-             this.leftRecordObject.mrCount = v.data.mrCount;
-             this.leftRecordObject.recipeSickName = v.data.recipeObject.recipeSickName;
-             this.leftRecordObject.mrIdCard = v.data.mrIdCard;
-             this.leftRecordObject.mrMcCard = v.data.mrMcCard;
-             this.leftRecordObject.sickPhone  = v.data.sickObject.sickPhone;
-             this.leftRecordObject.mrKsName   = v.data.mrKsName;
-             this.leftRecordObject.mrDoctorName = v.data.mrDoctorName;
-             this.leftRecordObject.mrTotalMoney  = v.data.mrTotalMoney;
-             this.leftRecordList = v.data;
-             //赋值集合 -- 表格显示
-             this.leftRecordListFunction(this.leftRecordList)
-           }).catch();
-         }
-       },
-       //项目总览，--赋值
-       leftRecordListFunction(row){
-          if(row.recipeObject.xpList[0].rdName !=null && ''!= row.recipeObject.xpList[0].rdName){
-            var sum1 = 0;//西药总价钱
-            row.recipeObject.xpList.forEach((drug,i)=>{//循环判断总价钱
-              sum1 += (drug.rdPrice*drug.rdCount);
-            })
-            this.xpObject.xmNumber = row.recipeObject.recipeNumber;
-            this.xpObject.xmName="西药处方";
-            this.xpObject.xmSum = sum1;
-            this.xpObject.xmText = row.recipeObject.xpNotes;
+		   selectRecord(){
+			 if(this.leftText!=null && this.leftText != ''){
+			   // 每次添加之前先清空一边集合
+			   this.axios.post("selectAllRecords",{texts:this.leftText}).then((v)=>{
+				if(v.data !=''){
+					
+					//赋值到个人信息去
+					this.leftRecordObject.mrCount = v.data.mrCount;
+					this.leftRecordObject.recipeSickName = v.data.recipeObject.recipeSickName;
+					this.leftRecordObject.mrIdCard = v.data.mrIdCard;
+					this.leftRecordObject.mrMcCard = v.data.mrMcCard;
+					this.leftRecordObject.sickPhone  = v.data.sickObject.sickPhone;
+					this.leftRecordObject.mrKsName   = v.data.mrKsName;
+					this.leftRecordObject.mrDoctorName = v.data.mrDoctorName;
+					this.leftRecordObject.mrTotalMoney  = v.data.mrTotalMoney;
+					this.leftRecordList = v.data;
+					//删除原有的数组中的数组
+					if(this.leftTableList.length>0 && this.leftTableList !=undefined){
+						this.leftTableList.forEach((a,i)=>{
+							a.xmContent.splice(i)
+						})
+					}
+					this.leftTableList=[]
+					//赋值集合 -- 表格显示
+					this.leftRecordListFunction(this.leftRecordList)
+					
+				}else{
+					this.$message({
+					  showClose: true,
+					  type: 'warning',
+					  message: '没有该患者的任何信息~'
+					});
+				}
 
-            row.recipeObject.xpList.forEach((b,i)=>{
-              //先添加一遍在清空数组
-              this.contentObject.coName = b.rdName;
-              this.contentObject.coPrice = b.rdPrice;
-              this.contentObject.coCount =b.rdCount;
-              this.contentArr1.push(this.contentObject)
-              //清空数组
-              this.contentObject={};
-            })
-            this.xpObject.xmContent=this.contentArr1
-            this.leftTableList.push(this.xpObject)
-          }
-         if(row.recipeObject.zpList[0].zpName !=null && '' != row.recipeObject.zpList[0].zpName){
-           var sum2 = 0;//中药总价钱
-           row.recipeObject.zpList.forEach((drug,i)=>{//循环判断总价钱
-             sum2 += (drug.zpPrice*drug.zpCount);
-           })
-           this.xpObject.xmNumber = row.recipeObject.recipeNumber;
-           this.zpObject.xmName="中药处方";
-           this.zpObject.xmSum = sum2;
-           this.zpObject.xmText = row.recipeObject.zpNotes;
+			   }).catch( );
+			 }
+		   },
+		   //项目总览，--赋值
+		   leftRecordListFunction(row){
+			  if(row.recipeObject.xpList[0].rdStatePrice == 0){
+				var sum1 = 0;//西药总价钱
+				row.recipeObject.xpList.forEach((drug,i)=>{//循环判断总价钱
+				  sum1 += (drug.rdPrice*drug.rdCount);
+				})
+				this.xpObject.xmNumber = row.recipeObject.recipeNumber;
+				this.xpObject.xmName="西药处方";
+				this.xpObject.xmSum = sum1;
+				this.xpObject.xmText = row.recipeObject.xpNotes;
 
-           row.recipeObject.zpList.forEach((b,i)=>{
-             //先添加一遍在清空数组
-             this.contentObject.coName = b.zpName;
-             this.contentObject.coPrice = b.zpPrice;
-             this.contentObject.coCount =b.zpCount;
-             this.contentArr2.push(this.contentObject)
-             //清空数组
-             this.contentObject={};
-           })
-           this.zpObject.xmContent=this.contentArr2
-           this.leftTableList.push(this.zpObject)
-         }
-         console.log(this.leftTableList)
-       },
-       forPrinting(index){
-         this.axios.post("forPrinting",{index:index}).then((v)=>{
-           console.log(v.data)
-         }).catch();
-       },
-       //标签方法
-       colorType(value) {
-         if(value=="西药处方" || value == "中药处方"){
-            return 'success';
-         }else{
-           return 'primary';
-         }
-       },
+				
+				row.recipeObject.xpList.forEach((b,i)=>{
+					if(b.rdStatePrice==0){
+						//先添加一遍在清空数组
+						this.contentObject.coName = b.rdName;
+						this.contentObject.coPrice = b.rdPrice;
+						this.contentObject.coCount =b.rdCount;
+						this.contentArr1.push(this.contentObject)
+						//清空数组
+						this.contentObject={};
+					}
+				})
+				this.xpObject.xmContent=this.contentArr1;
+				
+				this.leftTableList.push(this.xpObject)
+			  }
+			 if(row.recipeObject.zpList[0].zpStatePrice ==0){
+			   var sum2 = 0;//中药总价钱
+			   row.recipeObject.zpList.forEach((drug,i)=>{//循环判断总价钱
+				 sum2 += (drug.zpPrice*drug.zpCount);
+			   })
+			   this.zpObject.xmNumber = row.recipeObject.recipeNumber;
+			   this.zpObject.xmName="中药处方";
+			   this.zpObject.xmSum = sum2;
+			   this.zpObject.xmText = row.recipeObject.zpNotes;
 
-
+			   row.recipeObject.zpList.forEach((b,i)=>{
+				   if(b.zpStatePrice==0){
+					   //先添加一遍在清空数组
+					   this.contentObject.coName = b.zpName;
+					   this.contentObject.coPrice = b.zpPrice;
+					   this.contentObject.coCount =b.zpCount;
+					   this.contentArr2.push(this.contentObject)
+					   //清空数组
+					   this.contentObject={};
+				   }
+			   })
+			   this.zpObject.xmContent=this.contentArr2
+			   
+			   this.leftTableList.push(this.zpObject)
+			 }
+			 console.log(this.leftTableList)
+		   },
+		   //打印价格
+		   forPrinting(index,xmName){
+			   console.log(xmName)
+			 this.axios.post("forPrinting",{index:index,xmName:xmName}).then((v)=>{
+			   console.log(v.data)
+				if(v.data=='ok'){
+					this.selectRecord()	
+				}
+			   
+			 }).catch();
+		   },
+		   //标签方法
+		   colorType(value) {
+			 if(value=="西药处方" || value == "中药处方"){
+				return 'success';
+			 }else{
+			   return 'primary';
+			 }
+		   },
+		//查询所有的缴费记录总记录
+		selectRecordsAll(){
+			this.axios.post("selectRecordsAll",{text:this.rightText}).then((v)=>{
+				console.log(v.data)
+				this.rightList =v.data;
+			}).catch( );
 		},
+		
+		},
+		created(){
+			this.selectRecordsAll()
+		}
 		
 	};
 </script>
@@ -307,5 +350,11 @@
   color: red;
   font-size: 13px;
   cursor: pointer;
+}
+.fontType{
+  width: 80%;   /*一定要设置宽度，或者元素内含的百分比*/
+  overflow:hidden; /*溢出的部分隐藏*/
+  white-space: nowrap; /*文本不换行*/
+  text-overflow:ellipsis;/*ellipsis:文本溢出显示省略号（...）；clip：不显示省略标记（...），而是简单的裁切*/
 }
 </style>
--- src/components/outpatient/OutpatientLibrary.vue
+++ src/components/outpatient/OutpatientLibrary.vue
@@ -1,144 +1,272 @@
 <template>
-	<div></div>
 	<el-row>
 		<el-col :span="10">
-			<el-input style="width: 240px" v-model="input" placeholder="请输入患者信息方便查询" ></el-input>  
-			<el-button type="primary" icon="el-icon-search">查询</el-button>
+			<el-input style="width: 240px" v-model="text" placeholder="请输入患者信息方便查询" ></el-input>  
+			<el-button type="primary" icon="el-icon-search" @click="allRecordsSick">查询</el-button>
 		</el-col>
 	</el-row>
-<el-table
-	@row-dblclick="drawer = true" :data="tableData" border height="530" style="width: 100% ; margin-top: 10px">
-    <el-table-column
-      fixed
-      prop="date"
-      label="日期"
-      width="120">
-    </el-table-column>
-    <el-table-column
-      prop="name"
-      label="姓名"
-      width="100">
-    </el-table-column>
-    <el-table-column
-      prop="province"
-      label="省份"
-      width="100">
-    </el-table-column>
-    <el-table-column
-      prop="city"
-      label="市区"
-      width="100">
-    </el-table-column>
-    <el-table-column
-      prop="address"
-      label="地址"
-      width="600">
-    </el-table-column>
-    <el-table-column
-      prop="zip"
-      label="邮编"
-      width="120">
-    </el-table-column>
-    <el-table-column
-      fixed="right"
-      label="操作"
-      width="100">
-      <template #default="scope">
-        <el-button @click="handleClick(scope.row)" type="text" size="small">查看</el-button>
+<el-table size="mini" @row-dblclick="drawer = true" :data="allRecordList.slice((wardCurrentPage-1)*wardPageSize,wardCurrentPage*wardPageSize)"
+ border height="525" style="width: 100% ; margin-top: 10px">
+	
+    <el-table-column fixed prop="mrNumber" label="序号"  align="center" width="80px"></el-table-column>
+	
+	<el-table-column fixed prop="mrCount" label="就诊号" align="center" >
+		<template #default="scope">
+		  <el-tag  size="mini" type="warning"  disable-transitions>
+		    {{scope.row.mrCount}}
+		  </el-tag>
+		</template>
+	</el-table-column>
+	
+    <el-table-column  fixed label="姓名" align="center">
+      <template #default="scope" >
+        <el-popover effect="light" width="200px"  trigger="hover" placement="top">
+          <template #default >
+            <p>姓名: {{ scope.row.recipeObject.recipeSickName }}</p>
+            <p>身份证: {{ scope.row.sickObject.sickIdCard }}</p>
+			<p>就诊卡: {{ scope.row.mrMcCard }}</p>
+            <p>电话: {{ scope.row.sickObject.sickPhone }}</p>
+          </template>
+          <template #reference>
+            <div class="name-wrapper">
+              <el-tag size="medium">{{ scope.row.recipeObject.recipeSickName }}</el-tag>
+            </div>
+          </template>
+        </el-popover>
       </template>
     </el-table-column>
+	
+	<el-table-column  fixed label="挂号信息" align="center" >
+	  <template #default="scope" >
+	    <el-popover effect="light" width="100px"  trigger="hover" placement="top">
+	      <template #default >
+	        <p>排号: {{ scope.row.opcObject.bnCount }}</p>
+	        <p>挂号科室: {{ scope.row.opcObject.bnKsName }}</p>
+			<p>挂号医生: {{ scope.row.mrDoctorName }}</p>
+	        <p>挂号类型: {{ scope.row.opcObject.bnScience }}</p>
+	      </template>
+	      <template #reference>
+	        <div class="name-wrapper">
+	          <el-tag size="medium">{{ scope.row.opcObject.bnKsName }}</el-tag>
+	        </div>
+	      </template>
+	    </el-popover>
+	  </template>
+	</el-table-column>
+	
+	
+	
+	<el-table-column fixed label="就诊时间" align="center" >
+		<template #default="scope">
+		  <el-tag  size="mini" type="success"  disable-transitions>
+		    {{scope.row.mrSection}}
+		  </el-tag>
+		</template>
+	</el-table-column>
+	
+	<el-table-column fixed label="结束时间" align="center" >
+		<template #default="scope">
+		  <el-tag  size="mini"  type="danger" disable-transitions>
+		    {{scope.row.mrOverTime}}
+		  </el-tag>
+		</template>
+	</el-table-column>
+	
+	<el-table-column fixed prop="recipeObject.recipeDoctorName" label="诊断结果"  align="center">
+			  <template #default="scope" >
+			    <el-popover effect="light" width="300px"  trigger="hover" placement="top">
+			      <template #default >
+			        <p>主诉: {{ scope.row.historyObject.chComplaint }}</p>
+			        <p>体检结果: {{ scope.row.historyObject.chOe }}</p>
+					<p>现病史: {{ scope.row.historyObject.chCause }}</p>
+			        <p>处理和建议：{{ scope.row.historyObject.chDoctorText }}</p>
+			      </template>
+			      <template #reference>
+			        <div class="name-wrapper">
+			          <el-tag size="medium" class="fontType">{{ scope.row.historyObject.chDoctorText }}</el-tag>
+			        </div>
+			      </template>
+			    </el-popover>
+			  </template>
+	</el-table-column>
+	<el-table-column fixed label="总消费" align="center" >
+		<template #default="scope">
+		  <el-tag  size="mini"  type="danger" disable-transitions>
+		    {{scope.row.mrTotalMoney}}
+		  </el-tag>
+		</template>
+	</el-table-column>
+    <el-table-column fixed="right" label="操作"  align="center">
+      <template #default="scope">
+        <el-button @click="openDrawer(scope.row)" size="mini" type="primary" plain>查看</el-button>
+      </template>
+    </el-table-column>
+	
   </el-table>
   <div class="block">
-    <el-pagination
-		style="text-align: center; margin-top: 10px;"
-		background
-      @size-change="handleSizeChange"
-      @current-change="handleCurrentChange"
-      :current-page="currentPage4"
-      :page-sizes="[100, 200, 300, 400]"
-      :page-size="100"
-      layout="total, sizes, prev, pager, next, jumper"
-      :total="400">
-    </el-pagination>
+  <!--分页插件-->
+	  <el-pagination  @size-change="wardHandleSizeChange" @current-change="wardHandleCurrentChange"
+					  style="text-align: center; margin-top: 10px"
+					  :current-page="wardCurrentPage"
+					  :page-sizes="[2,4,6,8]"
+					  :page-size="wardPageSize"
+					  layout="total, sizes, prev, pager, next, jumper"
+					  :total="allRecordList.length">
+	  </el-pagination>
   </div>
 							<!-- ======================抽屉 -->
-  <el-drawer 			
-    title="我是外面的 Drawer"
-    v-model="drawer"
-    size="50%">
-    <div>
-     <el-button @click="innerDrawer = true">打开里面的!</el-button>
-     <el-drawer
-       title="我是里面的"
-       :append-to-body="true"
-       :before-close="handleClose"
-       v-model="innerDrawer">
-       <p>_(:зゝ∠)_</p>
-     </el-drawer>
-    </div>
-  </el-drawer>
+	<el-drawer :with-header="false" direction="ltr" v-model="drawer" size="50%" >
+		<el-row  style="margin-top: 10px;">
+			<el-col :span="12">
+				<h5>西药处方</h5>
+				<el-table  :summary-method="getSummaries1" show-summary  size="mini" :data="xpListTable" border height="350px" style="width: 100%">
+					<el-table-column align="center" prop="rdNumber" label="序号" width="50px" ></el-table-column>
+					<el-table-column align="center" prop="rdName" label="药名" width="120px" ></el-table-column>
+					<el-table-column align="center" prop="rdCount" label="数量"  width="50px"></el-table-column>
+					<el-table-column align="center" prop="rdPrice" label="单价" ></el-table-column>
+					<el-table-column align="center" label="总价" >
+						<template #default="scope">
+						    {{scope.row.rdCount*scope.row.rdPrice}}
+						</template>
+					</el-table-column>
+				</el-table>
+			</el-col>
+			<el-col :span="12">
+				<h5>中药处方</h5>
+				<el-table :summary-method="getSummaries2" show-summary size="mini"  :data="zpListTable" border height="350px" style="width: 100%">
+					<el-table-column align="center" prop="zpNumber" label="序号"  width="50px"></el-table-column>
+					<el-table-column align="center" prop="zpName" label="药名" width="120px" ></el-table-column>
+					<el-table-column align="center" prop="zpCount" label="数量"  width="50px"></el-table-column>
+					<el-table-column align="center" prop="zpPrice" label="单价" ></el-table-column>
+					<el-table-column align="center" label="总价" >
+						<template #default="scope">
+						    {{scope.row.zpCount*scope.row.zpPrice}}
+						</template>
+					</el-table-column>
+				</el-table>
+			</el-col>
+		</el-row>
+		<el-row style="margin-top: 10px;">
+			<el-col :span="12">
+				<h5>检验项目</h5>
+				<el-table size="mini" :data="testListTable" border height="300px" style="width: 100%">
+					<el-table-column prop="date" label="序号" width="180"></el-table-column>
+				</el-table>
+			</el-col>
+			<el-col :span="12">
+				<h5>手术项目</h5>
+				<el-table size="mini" :data="surgeryListTable" border height="300px" style="width: 100%">
+					<el-table-column prop="date" label="序号" width="180"></el-table-column>
+				</el-table>
+			</el-col>
+		</el-row>
+	</el-drawer>
 </template>
 
 <script>
 	export default {
 		data() {
 			return {
-				drawer: false,
-				innerDrawer: false,//抽屉
-				currentPage4: 4,
-				input:'',
-				tableData: [{
-				  date: '2016-05-02',
-				  name: '王小虎',
-				  province: '上海',
-				  city: '普陀区',
-				  address: '上海市普陀区金沙江路 1518 弄',
-				  zip: 200333
-				}, {
-				  date: '2016-05-04',
-				  name: '王小虎',
-				  province: '上海',
-				  city: '普陀区',
-				  address: '上海市普陀区金沙江路 1517 弄',
-				  zip: 200333
-				}, {
-				  date: '2016-05-01',
-				  name: '王小虎',
-				  province: '上海',
-				  city: '普陀区',
-				  address: '上海市普陀区金沙江路 1519 弄',
-				  zip: 200333
-				}, {
-				  date: '2016-05-03',
-				  name: '王小虎',
-				  province: '上海',
-				  city: '普陀区',
-				  address: '上海市普陀区金沙江路 1516 弄',
-				  zip: 200333
-				}]
+				drawer: false,//1层抽屉
+				innerDrawer: false,//2抽屉
+				wardCurrentPage:1,//分页属性
+				wardPageSize:4,
+				
+				//搜索text
+				text:'',
+				//集合
+				allRecordList:[],
+				//西药集合
+				xpListTable:[],
+				//中药集合
+				zpListTable:[],
+				//检验项目
+				testListTable:[],
+				//手术项目
+				surgeryListTable:[],
+				
+				
 			};
 		},
 		 methods: {
-			  handleClick(row) {
-				console.log(row);
-			  },
-			  handleSizeChange(val) {
-				console.log(`每页 ${val} 条`);
-			  },
-			  handleCurrentChange(val) {
-				console.log(`当前页: ${val}`);
-			  },
-				handleClose(done) {
-				this.$confirm('还有未保存的工作哦确定关闭吗？')
-				  .then(_ => {
-					done();
-				  })
-				  .catch(_ => {});
-				}
+			 // 初始病房每页数据数wardpagesize和数据data-----------------------分页方法------------------------------
+			 wardHandleSizeChange: function(size) {
+			   this.wardPageSize = size;
+			   console.log(this.pagesize) //每页下拉显示数据
+			 },
+			 //初始页病房wardcurrentPage
+			 wardHandleCurrentChange: function(currentPage) {
+			   this.wardCurrentPage = currentPage;
+			   console.log(this.currentPage) //点击第几页allDescSick
+			 },
+ 			//查询所有的当天就诊病人信息和处方记录详情
+			allRecordsSick(){
+				this.axios.post("allRecordsSick",{text:this.text}).then((v)=>{
+					console.log(v.data)
+					this.allRecordList =v.data;
+					console.log(this.allRecordList)
+				}).catch( );
+			},
+			//集合赋值
+			openDrawer(row){
+				this.drawer=true;
+				console.log(row)
+				//西药集合
+				this.xpListTable=row.recipeObject.xpList;
+				//中药集合
+				this.zpListTable=row.recipeObject.zpList;
+				// //检验项目
+				// this.testListTable=
+				// //手术项目
+				// this.surgeryListTable=
+			},
+			//计算总价
+			getSummaries1(param) {
+			  const { columns} = param;//获取到的整个表格的总栏位数
+			  const sums = [];//合计表个数组
+			  var sum = 0;//总价钱
+			  this.xpListTable.forEach((drug,i)=>{//循环判断总价钱
+			    sum += drug.rdPrice*drug.rdCount;
+			  })
+			  columns.forEach((column, index) => {//获取合计的位置
+			    if (index === 3) {
+			      sums[index] = "合计:";
+				  sums[index+1]= sum.toFixed(2);
+			      return;
+			    }
+			  });
+			  return sums;
+			},
+			getSummaries2(param) {
+			  const { columns} = param;//获取到的整个表格的总栏位数
+			  const sums = [];//合计表个数组
+			  var sum = 0;//总价钱
+			  this.zpListTable.forEach((drug,i)=>{//循环判断总价钱
+			    sum += drug.zpPrice*drug.zpCount;
+			  })
+			  columns.forEach((column, index) => {//获取合计的位置
+			    if (index === 3) {
+			      sums[index] = "合计:";
+				  sums[index+1]= sum.toFixed(2);
+			      return;
+			    }
+			  });
+			  return sums;
+			},
+		},
+		created(){
+			this.allRecordsSick()
 		},
 		
 	};
 </script>
 
-<style>
+<style scoped>
+	
+.fontType{
+  width: 80%;   /*一定要设置宽度，或者元素内含的百分比*/
+  overflow:hidden; /*溢出的部分隐藏*/
+  white-space: nowrap; /*文本不换行*/
+  text-overflow:ellipsis;/*ellipsis:文本溢出显示省略号（...）；clip：不显示省略标记（...），而是简单的裁切*/
+}
 </style>
