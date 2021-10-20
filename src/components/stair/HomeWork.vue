6a2ac5826e6eea5495144965af78df171f2354c3
yinyou <1337374876@qq.com>
2021-10-20 11:12:40 +0800

    整合修改bug9.3

--- src/components/stair/HomeWork.vue
+++ src/components/stair/HomeWork.vue
@@ -1,107 +1,103 @@
 <template>
-<!--	<h1>工作台</h1>-->
-<!--	<button @click="logout()">退出登入</button>-->
-  <span style="margin-left: 220px;font-size: 24px;color: red">本周药品采购</span>
-  <div id="myChart"
-       :style="{width: '50%',height:'400px'} " > </div>
+  <div>
+
+
+  <div id="app">
+    <div style="width: 300px;height: 100px;">
+      <i class="el-icon-user" style="font-size: 100px;"></i>
+      <div style="font-size: 20px;font-family:微软雅黑;color: #909399;margin-top: -80px;margin-left: 120px" >工作人员</div>
+      <div style="font-size: 20px;font-family:微软雅黑;color: black;margin-top: 10px;margin-left: 130px">{{funs}}</div>
+    </div>
+    <div style="width: 300px;height: 100px;margin-left: 500px;margin-top: -100px;">
+      <i class="el-icon-s-operation" style="font-size: 100px;"></i>
+      <div style="font-size: 20px;font-family:微软雅黑;margin-top: -80px;margin-left: 120px;color: #909399;" >就诊病例</div>
+      <div style="font-size: 20px;font-family:微软雅黑;color: black;margin-top: 10px;margin-left: 130px">{{nums}}</div>
+    </div>
+    <div style="width: 300px;height: 100px;margin-left: 1000px;margin-top: -100px">
+      <i class="el-icon-document-checked" style="font-size: 100px;"></i>
+      <div style="font-size: 20px;font-family:微软雅黑;color: #909399;margin-top: -80px;margin-left: 120px" >本月收入</div>
+      <div style="font-size: 20px;font-family:微软雅黑;color: black;margin-top: 10px;margin-left: 130px">{{moeney}}</div>
+    </div>
+    <div id="main" style="width:80%;height:60%;margin-top: 30px"></div>0
+  </div>
+  </div>
 </template>
-
 <script>
-import echarts from 'echarts'
-	export default {
-    data() {
-      return {
-        token: '',
-        tableData: [120, 132, 101, 134, 90, 230, 210]
-      }
+import echarts from "echarts";
+export default {
+  name: "App",
+  created() {
+    this.getAllEcharts();
+    this.dome()
+  },
+  data() {
+    return {
+      name: [],
+      num: [],
+      funs:"",
+      nums:'',
+      moeney:''
+    };
+  },
+  methods: {
+    async getAllEcharts() {
+      const {data: res1} = await this.$http.get("echarts");
+      console.log(res1);
+      this.num = res1.num;
+      this.name = res1.name;
+      console.log(this.num,"111")
+      this.setChart();
     },
-    methods: {
-      dome() {
-        //this.$root => app
-        let myChart = echarts.init(
-            document.getElementById("myChart")
-        );
-        //
-        // 绘制图表
-        myChart.setOption({
-          title: {
-            text: ''
+    setChart() {
+      // 基于准备好的dom，初始化echarts实例
+      this.chart = echarts.init(document.getElementById('main'))
+      // console.log(this.chart)
+      // 指定图表的配置项和数据
+      var option = {
+        title: {
+          text: "医院收入"
+        },
+        tooltip: {},
+        legend: {
+          data: ["住院部收入", '门诊部收入']
+        },
+        xAxis: {
+          data: ["1月","2月","3月","4月","5月","6月","7月","8月","9月","10月","11月","12月"],
+        },
+        yAxis: {},
+        series: [
+          {
+            name: "住院部收入",
+            type: "bar",
+            data: this.num,
           },
-          tooltip: {
-            trigger: 'axis'
+          {
+            name: '门诊部收入',
+            type: 'bar',
+            data: this.name
           },
-          legend: {
-            data: ['Email', 'Union Ads', 'Video Ads', 'Direct', 'Search Engine']
-          },
-          grid: {
-            left: '3%',
-            right: '4%',
-            bottom: '3%',
-            containLabel: true
-          },
-          toolbox: {
-            feature: {
-              saveAsImage: {}
-            }
-          },
-          xAxis: {
-            type: 'category',
-            boundaryGap: false,
-            data: ['Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat', 'Sun']
-          },
-          yAxis: {
-            type: 'value'
-          },
-          series: [
-            {
-              name: 'Email',
-              type: 'line',
-              stack: 'Total',
-              data: this.tableData
-            },
-            {
-              name: 'Union Ads',
-              type: 'line',
-              stack: 'Total',
-              data: [220, 182, 191, 234, 290, 330, 310]
-            },
-            {
-              name: 'Video Ads',
-              type: 'line',
-              stack: 'Total',
-              data: [150, 232, 201, 154, 190, 330, 410]
-            },
-            {
-              name: 'Direct',
-              type: 'line',
-              stack: 'Total',
-              data: [320, 332, 301, 334, 390, 330, 320]
-            },
-            {
-              name: 'Search Engine',
-              type: 'line',
-              stack: 'Total',
-              data: [820, 932, 901, 934, 1290, 1330, 1320]
-            }
-          ]
-        });
-        // console.log(myChart.setOption.title.text+'---')
-      },
-      logout(){
-        window.sessionStorage.clear()
-        this.$router.push('/')
+        ],
       }
-    },
-
-		created() {
-      this.token = this.$store.state.token
-
+      this.chart.setOption(option);
     },
     mounted() {
-      this.dome()
+      this.$nextTick(function () {
+        this.drawPid("main");
+      });
+    },
+    dome(){
+      this.axios.get("selectall-staffs").then((res)=>{
+        this.funs = res.data
+      }).catch()
+      this.axios.get("chaxunnums").then((res)=>{
+        this.nums=res.data
+      }).catch()
+      this.axios.get("moneny").then((res)=>{
+        this.moeney=res.data
+        // console.log(res,'1111')
+        console.log("1122",res)
+      }).catch()
     }
   }
+}
 </script>
-
-<style>
-</style>
