260e299faa60baf5d7ad0ae483967e3378740180
yinyou <1337374876@qq.com>
2021-10-20 11:03:16 +0800

    整合修改bug9.3

--- src/components/stair/HomeWork.vue
+++ src/components/stair/HomeWork.vue
@@ -14,7 +14,7 @@
       <div style="font-size: 20px;font-family:微软雅黑;color: black;margin-top: 10px;margin-left: 130px">{{nums}}</div>
     </div>
     <div style="width: 300px;height: 100px;margin-left: 1000px;margin-top: -100px">
-      <i class="el-icon-user" style="font-size: 100px;"></i>
+      <i class="el-icon-document-checked" style="font-size: 100px;"></i>
       <div style="font-size: 20px;font-family:微软雅黑;color: #909399;margin-top: -80px;margin-left: 120px" >本月收入</div>
       <div style="font-size: 20px;font-family:微软雅黑;color: black;margin-top: 10px;margin-left: 130px">{{moeney}}</div>
     </div>
